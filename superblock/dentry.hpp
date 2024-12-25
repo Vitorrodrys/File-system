9e5dc35ba22f009740b6b1fafe6ac42b9613902c
Vitor Rodrigues <vitorrodrys@gmail.com>
2024-12-25 15:36:05 -0300

    Refactor DEntry methods to be const-correct and update inode_map type

--- superblock/dentry.hpp
+++ superblock/dentry.hpp
@@ -13,16 +13,16 @@ class DEntry {
         DEntry();
         ~DEntry();
         static DEntry& load(const char& path); // TODO: check if we really will be receive a path file here
-        void save(const char& path); // TODO: check if we really will be receive a path file here
+        void save(const char& path) const; // TODO: check if we really will be receive a path file here
         bool add_entry(const char& path, InodeType inode );
         bool remove_entry(const char& path);
-        InodeType get_inode(const char& path);
-        bool is_directory(const char& path);
+        InodeType get_inode(const char& path) const;
+        bool is_directory(const char& path) const;
 
     private:
         //we will needs have more things here, such as the listing of free inodes, or other 
         //data structs to help us to manage us superblock
-        std::unordered_map<std::string, unsigned long long int> inode_map;
+        std::unordered_map<std::string, InodeType> inode_map;
         unsigned int quantity_entries;
 
         std::vector<char> serialize_to_bytes() const;
