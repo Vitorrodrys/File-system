d10aecf4215805bf24f841d348eecca601d4c278
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-01-25 22:30:39 -0300

    Refactor dentry to initializate only with slash inode

--- src/env.hpp
+++ src/env.hpp
@@ -6,9 +6,11 @@ class Env {
         Env();
 
     public:
+        const std::string& disk_file = std::getenv("DISK_FILE");
         const unsigned long long int disk_size = std::stoi(std::getenv("DISK_SIZE"));
         const unsigned int block_size = std::stoi(std::getenv("BLOCK_SIZE"));
         const unsigned long long int block_quantity = disk_size / block_size;
+        const unsigned int inode_slash = 200;
         // cannot allow copy and assigment
         Env(const Env&) = delete;
         Env& operator=(const Env&) = delete;
